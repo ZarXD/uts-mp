d07bd5c84ea86725b5092d1a00d09c2277be209a
ZarXD <banghads09@gmail.com>
2025-10-23 19:40:15 +0700

    Update

--- lib/login/main.dart
+++ lib/login/main.dart
@@ -8,82 +8,108 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+  final _showpassword = true;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.all(20.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
+      body: ListView(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                Container(
-                  height: 120,
-                  width: 120,
-                  decoration: BoxDecoration(
-                    image: DecorationImage(
-                      image: AssetImage("gambar/Logo.png"),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Container(
+                      height: 120,
+                      width: 120,
+                      decoration: BoxDecoration(
+                        image: DecorationImage(
+                          image: AssetImage("gambar/Logo.png"),
+                        ),
+                      ),
                     ),
-                  ),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        SizedBox(height: 8),
+                        Text(
+                          "LKS",
+                          style: TextStyle(
+                            fontSize: 32,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.blueAccent,
+                          ),
+                        ),
+                        Text(
+                          "MART",
+                          style: TextStyle(
+                            fontSize: 32,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.blueAccent,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
                 ),
+                SizedBox(height: 15),
                 Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.start,
                   children: [
-                    SizedBox(height: 8),
                     Text(
-                      "LKS",
+                      "Sign In",
                       style: TextStyle(
-                        fontSize: 32,
+                        fontSize: 24,
                         fontWeight: FontWeight.bold,
-                        color: Colors.blueAccent,
                       ),
                     ),
                     Text(
-                      "MART",
+                      "Enter your ID and Password to sign in!",
                       style: TextStyle(
-                        fontSize: 32,
-                        fontWeight: FontWeight.bold,
-                        color: Colors.blueAccent,
+                        fontSize: 19,
+                        fontWeight: FontWeight.w100,
+                        color: Colors.black26,
                       ),
                     ),
                   ],
                 ),
-              ],
-            ),
-            SizedBox(height: 15),
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: [
-                Text(
-                  "Sign In",
-                  style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-                ),
-                Text(
-                  "Enter your ID and Password to sign in!",
-                  style: TextStyle(
-                    fontSize: 19,
-                    fontWeight: FontWeight.w100,
-                    color: Colors.black26,
+                SizedBox(height: 30),
+                Text("Email"),
+                SizedBox(height: 8),
+                TextField(
+                  decoration: InputDecoration(
+                    hintText: "email",
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(8),
+                    ),
                   ),
                 ),
+                SizedBox(height: 15),
+                Text("Password*"),
+                SizedBox(height: 8),
+                TextField(
+                  obscureText: _showpassword,
+                  decoration: InputDecoration(
+                    hintText: "Min. 8 characters",
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(8),
+                    ),
+                    suffixIcon: Icon(Icons.remove_red_eye)
+                  ),
+                ),
+                Checkbox(value: true, onChanged: (value) {
+                  setState(() {
+                    
+                  });
+                },)
               ],
             ),
-            SizedBox(height: 30),
-            Text("Email"),
-            SizedBox(height: 8),
-            TextField(
-              decoration: InputDecoration(
-                hintText: "email",
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(8),
-                ),
-              ),
-            ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
